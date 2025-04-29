4188d4801c1f8287dc0eeef47c46e9b03f4d9862
ErnestSarna <ernest.sarna@ucalgary.ca>
2025-04-28 22:15:13 -0600

    added bootstrap and made basic navbar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './bootstrap.css';
 import './App.css';
 import NavBar from './NavBar.jsx';
 import Intro from './Intro.jsx';
@@ -7,7 +8,9 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <div style={{height: '40px', background: 'linear-gradient(to bottom, #350310, #282c34)'}}></div>
       <Intro />
+      <div style={{height: '40px', background: 'linear-gradient(to bottom, #282c34, #350310)'}}></div>
       <Projects />
     </div>
   );
--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,17 +1,21 @@
 
 function Intro(){
     return(
-        <div id="intro">
-            <div className="name">
-                <h1>Ernest Sarna</h1>
+        <div id="intro" className="py-5">
+            <div className="name me-5">
+                <h1 className="display-1">Ernest</h1>
+                <h1 className="display-1">Sarna</h1>
             </div>
-            <div className="description">
-                <p>
+            <div className="description mt-4">
+                <p className="lead">
                 Software Engineering graduate from the University of Calgary with a strong foundation in full-stack development and expertise in database management. 
                 Proficient in both front-end and back-end development, with a particular emphasis on front-end technologies. 
                 Demonstrates practical, hands-on experience through a portfolio of diverse software projects. 
                 Committed to continuous learning, driving impactful contributions, and fostering effective collaboration to ensure the success of team objectives.
                 </p>
+            </div>
+            <div className="skills">
+
             </div>
         </div>
     );
