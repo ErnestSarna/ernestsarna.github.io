f02c969bb2408d23a3ee5d5791d5a5322d3eb69e
ErnestSarna <ernest.sarna@ucalgary.ca>
2025-05-01 22:33:20 -0600

    footer design finalized, added resume download

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 
 function Footer(){
     return(
-        <div id="footer">
+        <div id="footer" className="pb-1">
             <div className="my-4">
                 <a className="ms-3" href="https://github.com/ErnestSarna" target="_blank" rel="noreferrer" title="GitHub">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
@@ -14,19 +14,21 @@ function Footer(){
                     </svg>
                 </a>
                 <a className="me-3" href="mailto:ernestsarna@gmail.com" target="_blank" rel="noreferrer" title="E-Mail">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
-                        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-envelope-at" viewBox="0 0 16 16">
+                        <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2zm3.708 6.208L1 11.105V5.383zM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2z"/>
+                        <path d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648m-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z"/>
                     </svg>
                 </a>
-                <a href="#" target="_blank" rel="noreferrer" title="Resume">
+                <a href="/Software_Resume.pdf" download="Ernest Sarna Resume" target="_blank" rel="noreferrer" title="Resume">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-file-earmark-person" viewBox="0 0 16 16">
                         <path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
                         <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2v9.255S12 12 8 12s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h5.5z"/>
                     </svg>
                 </a>
+
+                <p className="mt-3 mb-1">Built with the use of React and coded through Visual Studio Code. Styling done using Bootstrap.</p>
+                <p>Ernest Sarna ©2025</p>
             </div>
-            <p>Ernest Sarna ©2025</p>
-            <p>Built with the use of React through Visual Studio Code. Styling done using Bootstrap</p>
         </div>
     );
 }
